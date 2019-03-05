08cf9fdd652669287dcd27a8a59419bbb8c3206f
Royer2498 <royer_2498@hotmail.com>
2019-03-05 19:39:09 -0400

    add polymorphism

--- createStatementData.js
+++ createStatementData.js
@@ -5,25 +5,7 @@ class PerformanceCalculator{
     }
 
     get amount(){
-        let result = 0;
-        switch (this.aPlay.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.aPerformance.audience > 30) {
-                    result += 1000 * (this.aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.aPerformance.audience > 20) {
-                    result += 10000 + 500 * (this.aPerformance.audience - 20);
-                }
-                result += 300 * this.aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.aPerformance.play.type}`);
-        }
-        return result;
+        throw new Error('subclass responsibility');
     }
 
     get volumeCredits(){
@@ -36,6 +18,26 @@ class PerformanceCalculator{
     
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 40000;
+        if (this.aPerformance.audience > 30) {
+            result += 1000 * (this.aPerformance.audience - 30);
+        }
+        return result;
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 30000;
+        if (this.aPerformance.audience > 20) {
+            result += 10000 + 500 * (this.aPerformance.audience - 20);
+        }
+        result += 300 * this.aPerformance.audience;
+        return result;
+    }
+}
+
 export default function createStatementData(invoice,plays){
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -44,8 +46,19 @@ export default function createStatementData(invoice,plays){
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return statementData;
 
+    function createPerformanceCalculator(aPerformance,aPlay){
+        switch(aPlay.type){
+            case "tragedy":
+                return new TragedyCalculator(aPerformance,aPlay);
+            case "comedy":
+                return new ComedyCalculator(aPerformance,aPlay);
+            default:
+                throw new Error(`unknown type: ${aPlay.type}`);
+
+        }
+    }
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance,playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance,playFor(aPerformance));
         let result = Object.assign({}, aPerformance);
         result.play = calculator.aPlay;
         result.amount = calculator.amount;
@@ -64,6 +77,5 @@ export default function createStatementData(invoice,plays){
     function totalVolumeCredits(data){
         return data.performances.reduce( (total,performance) => total += performance.volumeCredits,0);
     }
-
 }
 
